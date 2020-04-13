6a148808f43b1368d4e2d0d04a054c9775cb92a0
Reo <yoshioka.web.d@gmail.com>
2020-04-13 13:19:29 +0900

    created the starategy page

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,38 @@
-//plug-in
-var gulp = require("gulp");
-// SASSファイルの読み込みに使用
-var sass = require("gulp-sass");
-// CSSの順番変更にに使用
-var csscomb = require("gulp-csscomb");
-// コンパイルに結果報告に使用
-var notify = require("gulp-notify");
-// 自動的にベンダープレフィックスの付与
-var autoprefixer = require("gulp-autoprefixer");
-// 自動的にブラウザ更新
-var browsersync = require("browser-sync").create();
-// soucemap生成
-var sourcemaps = require('gulp-sourcemaps');
 
-// サーバーを立ち上げる
-gulp.task('build-server', function (done) {
-    browsersync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    done();
-    console.log('Server was launched');
-});
+// gulp scss
 
-// ブラウザのリロード
-gulp.task('browser-reload', function (done){
-    browsersync.reload();
-    done();
-    console.log('Browser reload completed');
-});
+// npm init -y
+// npm install -D gulp
+// npm install -D gulp gulp-sass
 
-gulp.task("sass", function(done) {
-	gulp.src("./sass/**/*.scss")
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(csscomb())
-        .pipe(sourcemaps.write({includeContent: false}))
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(autoprefixer({
-        	browsers: ['last 2 versions']
-				}))
-        .pipe(sourcemaps.write('./'))
-				.pipe(gulp.dest("./"))
-        .pipe(notify('sassコンパイル完了'))
-				done();
-});
+// 実行 npx gulp
 
-// 監視ファイル
-gulp.task('watch-files', function(done) {
-    gulp.watch("./sass/**/*.scss", gulp.task('sass'));
-		gulp.watch("./sass/**/*.scss", gulp.task('browser-reload'));
-    gulp.watch("./**/*.html", gulp.task('browser-reload'));
-    done();
-    console.log(('gulp watch started'));
-});
+// gulpプラグインを読み込みます
+const { src, dest, watch } = require("gulp");
+// Sassをコンパイルするプラグインを読み込みます
+const sass = require("gulp-sass");
 
-// タスクの実行
-gulp.task('default', gulp.series('build-server', 'watch-files', function(done){
-    done();
-    console.log('Default all task done!');
-}));
+/**
+ * Sassをコンパイルするタスクです
+ */
+const compileSass = () =>
+  // style.scssファイルを取得
+  src("sass/style.scss")
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "compressed"
+      })
+    )
+    // cssフォルダー以下に保存
+    .pipe(dest("./"));
+
+/**
+ * Sassファイルを監視し、変更があったらSassを変換します
+ */
+const watchSassFiles = () => watch("./sass/**/*.scss", compileSass);
+
+// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
+exports.default = watchSassFiles;
+
---
+++ page-normal.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *Template Name: normal
+ *
+ * @package Glut
+ */
+
+get_header();
+ ?>
+
+
+
+
+<div class="p-normalLayout container-fluid">
+	<div class="container-md px-0">
+		<h2 class="p-normalLayout__title">
+			<?php the_title(); ?>
+			<span>GLUTのWEB戦略</span>
+		</h2>
+		<div class="p-normalLayout__breadcrumb">
+			<?php breadcrumb(); ?>
+		</div>
+	</div>
+</div>
+
+
+<main>
+	<section class="test">
+		<h2>MAKE THE VALUE</h3>
+		<h3>「成果を出せる価値あるWEB戦略を届ける」</h3>
+		<p>
+			この想いを念頭にGLUTでは2020年から制作業務をしています。
+			弊社では良い物とは3つの観点から定義をしています。
+		</p>
+	</section>
+	
+	<section class="test">
+		<img class="test__i" src="<?php echo get_template_directory_uri(); ?>/images/strategy01.jpg" alt="">
+		<h3 data-tooltip="01">「 成果に繋がる情報設計と洗練されたUI/UX」</h3>
+		<p>
+			良いユーザーインターフェースを作成する為にお客様が抱えている課題や
+			ユーザーが抱えている問題を抽出し、課題を解決するためにリサーチを行い、
+			ただデザインするだけではなく理論に基づいた情報設計を行います。
+		</p>
+	</section>
+
+	<section class="test">
+		<img class="test__i" src="<?php echo get_template_directory_uri(); ?>/images/strategy02.jpg" alt="">
+		<h3 data-tooltip="02">「Googleに準拠したSEOに強いデベロップメント」</h3>
+		<p>
+		Googleガイドラインに沿ったサイト構築をし、SEOに強いサイトをご提供します。
+		近年ではモバイルの設計やサイトスピードの最適化をすることで、
+		ユーザーの離脱を防ぐサイトを実現します。
+		</p>
+	</section>
+	<section class="test">
+		<img class="test__i" src="<?php echo get_template_directory_uri(); ?>/images/strategy03.jpg" alt="">
+		<h3 data-tooltip="03">「最適なマーケティングで成果を向上させる」</h3>
+		<p>
+		どんなに良い商品でも世の中に認知され、お客様に知って頂く事が重要です。
+		マーケティングは商品や企業様に応じたマーケティングが存在します。
+		我々はクライアント様にとって最適なマーケティング手法を洗い出しご提案しています。
+		</p>
+		<div class="test__button">
+			<a class="c-button" href="#">
+				実例を見る
+				<i class="fas fa-arrow-circle-right"></i>
+			</a>
+		</div>
+	</section>
+	
+</main>
+
+ <?php
+ get_footer();
