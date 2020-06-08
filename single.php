cd152e473e0d144582b0bbb1ffd24fc24f4c46f1
Reo <yoshioka.web.d@gmail.com>
2020-06-08 13:00:23 +0900

    シングルページデザイン変更

--- single.php
+++ single.php
@@ -33,18 +33,23 @@ $avatar_img  = scrapeImage( get_wp_user_avatar( $user_id ) );
 							<?php if(get_the_post_thumbnail()): ?>
 								<div class="p-entryContent__thumbnail" style="background-image:url(<?php echo get_the_post_thumbnail_url( $post->ID, 'large' ); ?>)"></div>
 							<?php endif; ?>
+					<div class="p-entryContent__date">
+						<time>
+							公開日:<?php the_time('Y/m/d');?> 　/　 最終更新日:<?php the_modified_date('Y/m/d') ?>
+						</time>
+					</div>
 					<h2 class="p-entryContent__title">
 						<?php the_title(); ?>
 					</h2>
 					<div class="p-entryContent__category">
-						<?php echo get_the_category()[0]->cat_name; ?>
+						<span><?php echo get_the_category()[0]->cat_name; ?></span> 
 					</div>
 					<div class="p-entryContent__author">
 						<div class="p-entryContent__author__img" style="background-image: url(<?php echo $avatar_img; ?>);"></div>
-						<div class="p-entryContent__author__name"><?php echo $author->display_name;?></div>
+						<div class="p-entryContent__author__name">記事を書いた人 : <?php echo $author->display_name;?></div>
 						<div class="p-entryContent__author__tag"><?php the_author_meta('user_description',$user_id); ?></div>
 					</div>
-
+					
 					<div class="entry-content">
 						<?php
 						the_content();
@@ -54,7 +59,7 @@ $avatar_img  = scrapeImage( get_wp_user_avatar( $user_id ) );
 				<?php endif; ?>
 
 					<div class="p-socialButton">
-						<h3>この記事を共有する</h3>
+						<h4>この記事を共有する</h4>
 						<a class="btn btn-icon btn-facebook" href="http://www.facebook.com/sharer.php?u=<?php echo urlencode(get_permalink()); ?>&amp;t=<?php echo urlencode(the_title("","",0)); ?>"><i class="fa fa-facebook"></i><span>Facebook</span></a>
 						<a class="btn btn-icon btn-twitter" href="http://twitter.com/intent/tweet?text=<?php echo urlencode(the_title("","",0)); ?>&amp;<?php echo urlencode(get_permalink()); ?>&amp;url=<?php echo urlencode(get_permalink()); ?>"><i class="fa fa-twitter"></i><span>Twitter</span></a>
 						<a class="btn btn-icon btn-line" href="http://line.me/R/msg/text/?<?php the_title(); ?>%0D%0A<?php the_permalink(); ?>"><i class="fab fa-line"></i><span>Line</span></a>
